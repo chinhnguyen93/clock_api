e4daf3d956d8b655e6c0d617855a8d8ee6b421f1
chinhnd9 <chinhnd9@tek-hand.com>
2021-04-13 12:04:52 +0700

    Updated follow friend api.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,11 +2,12 @@ class FriendsController < ApplicationController
   def create
     followee = User.find_by(id: params[:followee_id])
     return render json: { error: "Followee not found" }, status: 404 unless followee
-    friend = Friend.new(
+    friend = Friend.find_or_create_by(
       follower_id: @user.id,
       followee_id: params[:followee_id]
     )
-    render json: { message: "Follow successfull" }, status: 200 if friend.save
+    friend.update(is_deleted: false)
+    render json: { message: "Follow successfull" }, status: 200
   end
 
   def destroy
